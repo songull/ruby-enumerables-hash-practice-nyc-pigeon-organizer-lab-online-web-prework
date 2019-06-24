ceb9b148aa0a3ed900f40f16c588b84acb41a83e
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-24 00:51:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -36,4 +36,18 @@ def nyc_pigeon_organizer(data)
       :lives => ["Central Park"]
     }
   }
+  pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
 end
