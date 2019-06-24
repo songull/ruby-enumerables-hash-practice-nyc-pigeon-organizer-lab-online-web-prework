1bc8bd7087d2043e05c4e65c9f4ff93160abacba
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-24 00:48:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,39 @@
 def nyc_pigeon_organizer(data)
-  pigeons = {
-    :color => {
-      :brown => ["Alex"],
-      :white => ["Alex"],
+  pigeon_list = {
+    "Theo" => {
+      :color => ["purple", "grey"],
+      :gender => ["male"],
+      :lives => ["Subway"]
     },
-    :gender => {
-      :male => ["Alex"],
-      :female => ["Lola"]
+    "Peter Jr." => {
+      :color => ["purple", "grey"],
+      :gender => ["male"],
+      :lives => ["Library"]
     },
-    :lives => {
-      "Central Park" => ["Alex"],
-      "Library" => ["Luca"]
+    "Lucky" => {
+      :color => ["purple"],
+      :gender => ["male"],
+      :lives => ["Central Park"]
+    },
+    "Ms. K" => {
+      :color => ["grey", "white"],
+      :gender => ["female"],
+      :lives => ["Central Park"]
+    },
+    "Queenie" => {
+      :color => ["white", "brown"],
+      :gender => ["female"],
+      :lives => ["Subway"]
+    },
+    "Andrew" => {
+      :color => ["white"],
+      :gender => ["male"],
+      :lives => ["City Hall"]
+    },
+    "Alex" => {
+      :color => ["white", "brown"],
+      :gender => ["male"],
+      :lives => ["Central Park"]
     }
   }
 end
